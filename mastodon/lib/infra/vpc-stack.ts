423b66b3d73a6682ac3bab5cfa85450dcf76e3e8
Sakai Takao <s.takao.73+github@gmail.com>
2024-07-08 17:42:35 +0900

    NAT Instance のセキュリティを強化

--- mastodon/lib/infra/vpc-stack.ts
+++ mastodon/lib/infra/vpc-stack.ts
@@ -1,20 +1,29 @@
-import { aws_ec2 } from 'aws-cdk-lib';
-import { SubnetType, GatewayVpcEndpointAwsService, NatInstanceProviderV2 } from 'aws-cdk-lib/aws-ec2';
+import {
+  SubnetType,
+  GatewayVpcEndpointAwsService,
+  NatInstanceProviderV2,
+  Port,
+  Peer,
+  Vpc,
+  IpAddresses,
+  IpProtocol,
+  InterfaceVpcEndpointAwsService,
+} from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class VpcStack extends Construct {
-  public readonly vpc: aws_ec2.Vpc;
+  public readonly vpc: Vpc;
 
   constructor(scope: Construct, natGatewayProvider: NatInstanceProviderV2) {
     super(scope, 'vpc');
 
-    const vpc = new aws_ec2.Vpc(this, 'mastodon-infra-vpc', {
-      ipAddresses: aws_ec2.IpAddresses.cidr(process.env.VPC_CIDR!),
+    const vpc = new Vpc(this, 'mastodon-infra-vpc', {
+      ipAddresses: IpAddresses.cidr(process.env.VPC_CIDR!),
       vpcName: 'mastodon-infra-vpc',
       natGatewayProvider,
       natGateways: 1,
       maxAzs: 2,
-      ipProtocol: aws_ec2.IpProtocol.DUAL_STACK,
+      ipProtocol: IpProtocol.DUAL_STACK,
       subnetConfiguration: [
         {
           subnetType: SubnetType.PUBLIC,
@@ -36,6 +45,8 @@ export class VpcStack extends Construct {
         },
       },
     });
+
+    natGatewayProvider.securityGroup.addIngressRule(Peer.ipv4(vpc.vpcCidrBlock), Port.allTraffic());
 
     this.vpc = vpc;
   }
