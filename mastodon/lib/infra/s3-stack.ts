40efa4cd8a84476057eda32b68a6e0aa3175dfd9
FullStuck <git@fullstuck.net>
2025-03-25 18:28:28 +0900

    S3 の public access を無効にする

--- mastodon/lib/infra/s3-stack.ts
+++ mastodon/lib/infra/s3-stack.ts
@@ -1,5 +1,6 @@
 import { RemovalPolicy } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { AccountPrincipal, AccountRootPrincipal, ArnPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { BlockPublicAccess, Bucket, CfnBucketPolicy } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class S3Stack extends Construct {
@@ -13,10 +14,22 @@ export class S3Stack extends Construct {
     // S3
     this.backyard = new Bucket(this, 'mastodon-infra-s3-backyard', {
       removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
+
     this.accessLog = new Bucket(this, 'mastodon-infra-s3-access-log', {
       removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
+    this.accessLog.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ['s3:PutObject'],
+        effect: Effect.ALLOW,
+        principals: [new AccountPrincipal('127311923021')],
+        resources: [this.accessLog.arnForObjects('*')],
+      }),
+    );
+
     this.contents = new Bucket(this, 'mastodon-infra-s3-contents', {
       removalPolicy: RemovalPolicy.DESTROY,
     });
