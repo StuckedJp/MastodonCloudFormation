e7822acacddd1d27a3b5d7e3d696335a2e84107e
FullStuck <git@fullstuck.net>
2025-03-27 19:43:40 +0900

    CloudFrontConstruct -> AttachmentDistributionConstruct

--- mastodon/lib/infra/mastodon-infra-stack.ts
+++ mastodon/lib/infra/mastodon-infra-stack.ts
@@ -3,13 +3,13 @@ import { Construct } from 'constructs';
 import { S3Construct } from './s3.construct';
 import { VpcConstruct } from './vpc.construct';
 import { NatInstanceConstruct } from './nat-instance.construct';
-import { CloudFrontConstruct } from './cloud-front.construct';
+import { AttachmentDistributionConstruct } from './attachment-dist.construct';
 
 export class MastodonInfraStack extends cdk.Stack {
   public readonly natGatewayProvider: NatInstanceConstruct;
   public readonly vpc: VpcConstruct;
   public readonly s3: S3Construct;
-  public readonly cloudFront: CloudFrontConstruct;
+  public readonly cloudFront: AttachmentDistributionConstruct;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -17,6 +17,6 @@ export class MastodonInfraStack extends cdk.Stack {
     this.natGatewayProvider = new NatInstanceConstruct(this);
     this.vpc = new VpcConstruct(this, this.natGatewayProvider.natGatewayProvider);
     this.s3 = new S3Construct(this);
-    this.cloudFront = new CloudFrontConstruct(this, this.s3.contents)
+    this.cloudFront = new AttachmentDistributionConstruct(this, this.s3.contents)
   }
 }
