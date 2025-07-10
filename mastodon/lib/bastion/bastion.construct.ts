c214e2da2ab9d2b590f31de98d3aa68bacbe7d32
FullStuck <git@fullstuck.net>
2025-07-10 20:48:11 +0900

    更新

--- mastodon/lib/bastion/bastion.construct.ts
+++ mastodon/lib/bastion/bastion.construct.ts
@@ -93,7 +93,7 @@ export class BastionConstruct extends Construct {
       // Node.js
       `mkdir -p /etc/apt/keyrings`,
       `curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg`,
-      `NODE_MAJOR=20`,
+      `NODE_MAJOR=${process.env.NODE_VERSION}`,
       `echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list`,
       `apt-get update`,
       `apt-get install -y nodejs`,
